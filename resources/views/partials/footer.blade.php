9bd18abcedd846f678a2318826f77dc367ac4d50
Abraxas-7 <arber.beshaj.02@gmail.com>
2025-03-26 18:18:58 +0100

    Add footer banners with styles and update layout for enhanced user engagement

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -59,6 +59,30 @@
     </div>
 
     <div class="bottom-banner">
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class="left-bottom-banner d-flex justify-content-between align-items-center">
+                <a href="#">
+                    <h5>SING-UP NOW!</h5>
+                </a>
+            </div>
 
+            <div class="right-bottom-banner d-flex justify-content-between align-items-center">
+                <a href="#">
+                    <h4>FOLLOW US</h4>
+                </a>
+                <div class="d-flex justify-content-between align-items-center">
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}"
+                            alt="facebook"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}"
+                            alt="twitter"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}"
+                            alt="youtube"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}"
+                            alt="pinterest"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}"
+                            alt="periscope"></a>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
